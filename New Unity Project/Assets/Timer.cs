74940cfd446bb2aaf91541482268af83a4b811bb
ivypwu <1989ivy66@gmail.com>
2016-06-04 21:34:02 -0700

    fixed untracked files

--- New Unity Project/Assets/Timer.cs
+++ New Unity Project/Assets/Timer.cs
@@ -5,30 +5,32 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
 
-    public float min = 0;
-    public float second = 0;
+    public float Starthour = 0;
+
+    public float Endhour = 0;
+
+
     private float time;
+    private float Endtime;
     public Text CountText;
+
     // Use this for initialization
     void Start () {
-        time = min * 60 + second;
+        /*game 1 min = real world 1 second*/
+        float hour = Endhour - Starthour;
+        Endtime = hour * 60;
 
-        /*we are NOT making a hour-level*/
-        if(min > 60)
-        {
-            min = 60;
-        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         /*update time*/
-        time -= Time.deltaTime;
+        time += Time.deltaTime;
 
-        second = Mathf.Floor(time) % 60;
-        min = (Mathf.Floor(time) - second) / 60;
+        float min = Mathf.Floor(time) % 60;
+        float hour = (Mathf.Floor(time) - min) / 60;
 
-        if(min < 2)
+        if(hour < 2)
         {
             /*this is bad but whatever*/
             CountText.color = Color.red;
@@ -36,11 +38,11 @@ public class Timer : MonoBehaviour {
 
         /*show the value on the screen*/
         string buffer = "";
-        if(second <10)
+        if(min < 10)
         {
             buffer = "0";
         }
 
-        CountText.text = "Time : \n" + min.ToString() + ":" + buffer + second.ToString();
+        CountText.text = "Time : \n" + hour.ToString() + ":" + buffer + min.ToString();
     }
 }
